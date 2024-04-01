32e9240f6516a4eb439e39b89db5cb6f031e5dca
turma19 <marianachristine31@gmail.com>
2024-04-01 17:10:14 -0300

    codigo de comando basico

---
+++ src/test/java/Comandosbasicos.java
@@ -0,0 +1,21 @@
+
+public class Comandosbasicos {
+
+	public static void main(String[] args) {
+		
+	//  Tipo de Variável
+		String nome = "Mariana Reis ";
+		int idade = 17;
+		double peso = 56.6;
+		double altura = 1.58; 
+		boolean podeounaopode = true;
+		System.out.println( nome + podeounaopode );
+		
+		
+		System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos" + ", tenho " + peso + " kilos" + " e tenho " + altura + " de altura ");
+	
+		
+		
+	}
+
+}
